420bdb52ff4e256b013a9657eb9857e49f8dd2d5
Cook1619 <cook6348@gmail.com>
2018-06-17 20:02:50 -0500

    Have the logic set up, need to do css and add the static posts

---
+++ src/ThreadDisplay/component/ThreadDisplay.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Post from '../../Post/component/Post';
+import PostEditor from '../../PostEditor/component/PostEditor'
+
+class ThreadDisplay extends Component {
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            posts: [],
+
+        }
+    }
+    addPost = (newPostBody) => {
+        const newState = Object.assign({}, this.state);
+        newState.posts.push(newPostBody);
+        this.setState(newState);
+    }
+    render() {
+        return (
+            <div>
+                {
+                    this.state.posts.map((postBody, index) => {
+                        return (
+                            <Post key={index} postBody={postBody} />
+                        )
+                    })}
+
+                <PostEditor addPost={this.addPost} />
+            </div>
+        )
+    }
+}
+
+export default ThreadDisplay;
